badc698df63928fabe8b0c0cbe05cbe7215e024f
nael-s <nael-s@mgdis.fr>
2015-11-13 13:59:18 +0100

    On bouge des carrés avec les sockets \o/

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('http').createServer()
 var io = require('socket.io')(app);
 var fs = require('fs');
+var max = 250;
+var min = 0;
 
 app.listen(3000);
 var usersConnected = [];
@@ -8,22 +10,50 @@ var simplifiedUsers = [];
 
 io.sockets.on('connection', function (socket) {
 	usersConnected.push(socket);
-	simplifiedUsers.push(socket.client.conn.id);
+	var client = {
+		id : socket.client.conn.id,
+		posX : Math.floor(Math.random() * (max - min) + min),
+		posY : Math.floor(Math.random() * (max - min) + min)
+	}
+	simplifiedUsers.push(client);
 	console.log("user connected (" + usersConnected.length + ")");
-  
-  	socket.broadcast.emit('userConnected', simplifiedUsers);
 
-  	socket.on('disconnect', function(){
-  		var index = usersConnected.indexOf(socket);
-  		usersConnected.splice(index, 1);
-  		simplifiedUsers.splice(index,1);
-  		console.log("user disconnected ("+usersConnected.length+")");
-  	});
+	socket.emit('userConnected', simplifiedUsers);
+	socket.broadcast.emit('userConnected', simplifiedUsers);
+
+	socket.on('connected', function(data){
+		console.log(data);
+		var index = usersConnected.indexOf(socket);
+		simplifiedUsers[index].posX = data.posX;
+		simplifiedUsers[index].posY = data.posY;
+		socket.broadcast.emit('userConnected', simplifiedUsers);
+		console.log(simplifiedUsers);
+	});
+
+	socket.on('update', function(data){
+		console.log("Oui ?");
+		var index = usersConnected.indexOf(socket);
+		console.log(simplifiedUsers[index]);
+		simplifiedUsers[index].posX = data.posX;
+		simplifiedUsers[index].posY = data.posY;
+		console.log(simplifiedUsers[index]);
+
+		socket.emit('userConnected', simplifiedUsers);
+		socket.broadcast.emit('userConnected', simplifiedUsers);
+	});
+
+	socket.on('disconnect', function(){
+		var index = usersConnected.indexOf(socket);
+		usersConnected.splice(index, 1);
+		simplifiedUsers.splice(index,1);
+		console.log("user disconnected ("+usersConnected.length+")");
+		socket.broadcast.emit('userConnected', simplifiedUsers);
+	});
 });
 
 setInterval(function(){
-	console.log("number of users : " + usersConnected.length);
-	for (var i = usersConnected.length - 1; i >= 0; i--) {
-		console.log("=> " + usersConnected[i].client.conn.id);
+	console.log("number of users : " + usersConnected.length, simplifiedUsers);
+	for (var i = simplifiedUsers.length - 1; i >= 0; i--) {
+		console.log("=> " + simplifiedUsers[i].id);
 	};
-}, 1000);
+}, 5000);
--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,60 @@
 var socket = io('http://localhost:3000');
-	socket.on('news', function (data) {
-	console.log(data);
-	socket.emit('my other event', { my: 'data' });
-});
+var width = 10;
+var max = 250;
+var min = 0;
+var clients = [];
+var me = {};
 
 socket.on('userConnected', function(data){
-	console.log(data.length);
+	clients = data;
+	$("#userList").empty();
 	for (var i = data.length - 1; i >= 0; i--) {
-		console.log(i);
-		$("#userList").append("<li>"+data[i]+"</li>");
+		$("#userList").append("<li>"+data[i].id+"</li>");
 	};
+
+	me = clients.find(currentSocket);
+
+	updateCanvas();
 });
-
-$("#userList").append("<li>Coucou ! </li>");
-
-window.onLoad = function(){
-	socket.emit('connection');
-};
 
 window.onBeforeUnload = function(){
 	socket.disconnect();
 }
+
+// canvas
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+updateCanvas = function(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	for (var i = 0; i < clients.length; i++) {
+		ctx.fillRect(clients[i].posX, clients[i].posY, width, width);
+	}
+};
+
+window.addEventListener("keydown", function (event) {
+
+	if (event.defaultPrevented) {
+		return; // Should do nothing if the key event was already consumed.
+	}
+	switch (event.keyIdentifier) {
+    case "Down":
+			me.posY +=1;
+			break;
+    case "Up":
+			me.posY -=1;
+    	break;
+    case "Left":
+			me.posX -=1;
+    	break;
+    case "Right":
+			me.posX +=1;
+    	break;
+	}
+	socket.emit('update', me);
+});
+
+
+currentSocket = function(element, index, array){
+	return element.id == socket.id;
+};
